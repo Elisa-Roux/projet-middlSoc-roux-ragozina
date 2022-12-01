aa8227e5939a0b7413c104e3a10212f40e15cdb3
yana <yana.ragozina@etu.unice.fr>
2022-12-01 22:08:00 +0100

    client java

---
+++ ClientSOAP/src/main/java/com/soap/test/Test.java
@@ -0,0 +1,22 @@
+package com.soap.test;
+
+import com.soap.ws.client.generated.*;
+
+public class Test {
+
+    //COMMENT LE LANCER AU TOUT DEBUT :
+    //mvn clean jaxws:wsimport
+    //puis Build si ca marche toujours pas
+
+    //Si maven trouve pas les plugins
+    /*https://stackoverflow.com/questions/20496239/maven-plugins-can-not-be-found-in-intellij*/
+
+    //si "couldn't find dependency/cannot resolve symbol dans les imports => File/Invalidate
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! we are going to test a SOAP client written in Java");
+        LetsGoBikingService l = new LetsGoBikingService();
+        ILetsGoBikingService i = l.getBasicHttpBindingILetsGoBikingService();
+        System.out.println(i.getItinerary("2400 route des dolines", "aix en provence"));
+    }
+}
---
+++ ClientSOAP/src/main/java/com/soap/ws/client/generated/Add.java
@@ -0,0 +1,89 @@
+
+package com.soap.ws.client.generated;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="left" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/&gt;
+ *         &lt;element name="right" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "left",
+    "right"
+})
+@XmlRootElement(name = "Add")
+public class Add {
+
+    protected Integer left;
+    protected Integer right;
+
+    /**
+     * Gets the value of the left property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getLeft() {
+        return left;
+    }
+
+    /**
+     * Sets the value of the left property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setLeft(Integer value) {
+        this.left = value;
+    }
+
+    /**
+     * Gets the value of the right property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getRight() {
+        return right;
+    }
+
+    /**
+     * Sets the value of the right property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setRight(Integer value) {
+        this.right = value;
+    }
+
+}
